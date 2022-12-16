a7f09bdd3907d59a75b4e685c0be86b0c5cc71fa
Vitaly Legchilkin <vlegchilkin@gmail.com>
2022-12-16 19:26:47 +0100

    Day 16: modify template (+ignore) + renaming

--- aoc_2022/day_16.py
+++ aoc_2022/day_16.py
@@ -13,27 +13,26 @@ def f(src_valve, time, time_next: Optional[int]) -> int:
         if time > (ti := costs[src_valve][dst_valve]):
             closed_valves.remove(dst_valve)
             ti = time - ti
-            best = max(best, ti * valves[dst_valve] + f(dst_valve, ti, time_next))
+            best = max(best, ti * rates[dst_valve] + f(dst_valve, ti, time_next))
             closed_valves.add(dst_valve)
 
     return best
 
 
 ttp_template = """\
-Valve {{ src }} has flow rate={{ rate | to_int }}; {{ dst | ORPHRASE | split(", ") }}
+Valve {{ src }} has flow rate={{ rate | to_int }}; \
+{{ ignore(r"tunnel(s)? lead(s)? to valve(s)?") }} {{dst | ORPHRASE | split(", ")}}
 """
 
 if __name__ == "__main__":
     input_data = {d.src: (d.rate, d.dst) for d in Input().get_objects(ttp_template)}
-    for _, dst in input_data.values():
-        dst[0] = dst[0].rpartition(" ")[-1]
 
     keys = sorted(input_data)
-    valves = [input_data[k][0] for k in keys]
+    rates = [input_data[k][0] for k in keys]
     links = {keys.index(k): [(keys.index(d), 1) for d in input_data[k][1]] for k in keys}
 
     costs = Graph(links).floyd().add_weight(1).to_list()
-    closed_valves = {i for i, valve in enumerate(valves) if valve > 0}
+    closed_valves = {i for i, valve in enumerate(rates) if valve > 0}
 
     start_time = timer.time()
     print(f"part_a: {f(0, 30, None)}")
