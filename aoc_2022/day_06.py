5e98bb98a0c1619473dd34c8cbbb0f7da3db2dcd
Vitaly Legchilkin <vlegchilkin@gmail.com>
2022-12-17 17:53:00 +0100

    Day 5 & 6 tests

--- aoc_2022/day_06.py
+++ aoc_2022/day_06.py
@@ -1,13 +1,29 @@
 from aoc_2022 import Input
 
 
-def find_unique(text, length) -> int:
-    for i in range(length, len(text)):
-        if len(set(text[i - length : i])) == length:
-            return i
+class Solution:
+    def __init__(self, inp: Input):
+        self.line = inp.get_lines()[0]
+
+    def find_unique(self, length) -> int:
+        for i in range(length, len(self.line)):
+            if len(set(self.line[i - length : i])) == length:
+                return i
+
+    def part_a(self):
+        return self.find_unique(4)
+
+    def part_b(self):
+        return self.find_unique(14)
 
 
-if __name__ == "__main__":
-    line = Input().get_lines()[0]
-    print(f"part_a: {find_unique(line, 4)}")
-    print(f"part_a: {find_unique(line, 14)}")
+def test_simple():
+    solution = Solution(Input(0))
+    assert solution.part_a() == 7
+    assert solution.part_b() == 19
+
+
+def test_challenge():
+    solution = Solution(Input())
+    assert solution.part_a() == 1625
+    assert solution.part_b() == 2250
