edd92afc443a1c2c8451e99522723e5f4d88e594
Vitaly Legchilkin <vlegchilkin@gmail.com>
2022-12-18 18:29:13 +0100

    Day 18 (#4)
    Day 18 - two options -[Graphs / BFS]- for part b

---
+++ aoc_2022/day_18.py
@@ -0,0 +1,111 @@
+from collections import deque
+from itertools import combinations
+from typing import Optional
+
+import networkx as nx
+import numpy as np
+
+from aoc_2022 import Input, t_sum, t_delta, t_minmax, t_inside_array
+
+# 6 cube sides with 4 vertexes of each side
+SIDE_VERTEXES = {
+    (0, 0, -1): [(0, 0, 0), (1, 0, 0), (1, 1, 0), (0, 1, 0)],
+    (-1, 0, 0): [(0, 0, 0), (0, 0, 1), (0, 1, 1), (0, 1, 0)],
+    (0, -1, 0): [(0, 0, 0), (1, 0, 0), (1, 0, 1), (0, 0, 1)],
+    (0, 0, 1): [(0, 0, 1), (1, 0, 1), (1, 1, 1), (0, 1, 1)],
+    (1, 0, 0): [(1, 0, 0), (1, 0, 1), (1, 1, 1), (1, 1, 0)],
+    (0, 1, 0): [(0, 1, 0), (1, 1, 0), (1, 1, 1), (0, 1, 1)],
+}
+
+
+class Solution:
+    def __init__(self, inp: Input):
+        self.cubes = set([tuple([int(c) for c in line.split(",")]) for line in inp.get_lines()])
+
+    def _get_clean_sides(self):
+        clean = []
+        for cube in self.cubes:
+            for t in SIDE_VERTEXES:
+                if tuple(map(sum, zip(cube, t))) not in self.cubes:
+                    clean.append((cube, t))
+        return clean
+
+    @staticmethod
+    def _get_sides_common_edge(a, b) -> Optional:
+        cube_a, side_a = a
+        cube_b, side_b = b
+
+        if sum(d := t_delta(cube_a, cube_b)) > 2 or max(d) > 1:
+            return  # cubes a & b too far away to have a common edge
+
+        if side_a[0] + side_b[0] == side_a[1] + side_b[1] == side_a[2] + side_b[2] == 0:
+            return  # opposite sides of cubes can't be linked
+
+        vertexes_a = set([t_sum(cube_a, p) for p in SIDE_VERTEXES[side_a]])
+        vertexes_b = set([t_sum(cube_b, p) for p in SIDE_VERTEXES[side_b]])
+        if len(common := vertexes_a & vertexes_b) == 2:
+            return tuple(sorted(common))  # cubes touched within an edge in 2 common points
+
+    def part_a(self):
+        return len(self._get_clean_sides())
+
+    def part_b_graphs(self):
+        clean_sides = self._get_clean_sides()
+        side_edges = dict()
+        for a, b in combinations(clean_sides, 2):
+            if edge := self._get_sides_common_edge(a, b):
+                # sides from the same cube has low priority in links for each edge
+                la = side_edges.setdefault(a, {})
+                if not (existent := la.get(edge)) or existent[0] == a[0]:
+                    la[edge] = b
+                lb = side_edges.setdefault(b, {})
+                if not (existent := lb.get(edge)) or existent[0] == b[0]:
+                    lb[edge] = a
+
+        graph = nx.Graph()
+        for side_a, edges in side_edges.items():
+            for side_b in edges.values():
+                graph.add_edge(side_a, side_b)
+
+        connected_components = list(nx.connected_components(graph))
+
+        return max([len(c) for c in connected_components])
+
+    def part_b_bfs(self):
+        mm = t_minmax(self.cubes)
+        real_zero = t_sum(mm[0], (-1, -1, -1))
+        visited = np.zeros(t_sum(t_delta(*mm), (3, 3, 3)), dtype=int)
+
+        queue, visible, source = deque(), 0, (0, 0, 0)
+        visited[source] = 1
+        queue.append(source)
+
+        while queue:
+            pos = queue.popleft()
+            for side in SIDE_VERTEXES:
+                next_pos = t_sum(pos, side)
+                if t_inside_array(next_pos, visited.shape) and not visited[next_pos]:
+                    if t_sum(next_pos, real_zero) in self.cubes:
+                        visible += 1
+                    else:
+                        visited[next_pos] = 1
+                        queue.append(next_pos)
+        return visible
+
+
+def test_simple():
+    solution = Solution(Input(0))
+    assert solution.part_a() == 64
+    assert solution.part_b_graphs() == solution.part_b_bfs() == 58
+
+
+def test_ideal():
+    solution = Solution(Input("ideal"))
+    assert solution.part_a() == 36
+    assert solution.part_b_graphs() == solution.part_b_bfs() == 30
+
+
+def test_challenge():
+    solution = Solution(Input())
+    assert solution.part_a() == 4364
+    assert solution.part_b_graphs() == solution.part_b_bfs() == 2508
