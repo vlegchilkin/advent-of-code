f7e1a802079992faf3b717d4460bd9fc2f9a69c4
Vitaly Legchilkin <vlegchilkin@gmail.com>
2022-12-18 16:18:15 +0100

    Day 18 - Cleanup

--- aoc_2022/day_18.py
+++ aoc_2022/day_18.py
@@ -1,73 +1,67 @@
+from itertools import combinations
 from typing import Optional
 
 from aoc_2022 import Input
 import networkx as nx
 
-D = [
-    [(0, 0, 0), (1, 0, 0), (1, 1, 0), (0, 1, 0)],
-    [(0, 0, 0), (0, 0, 1), (0, 1, 1), (0, 1, 0)],
-    [(0, 0, 0), (1, 0, 0), (1, 0, 1), (0, 0, 1)],
-    [(0, 0, 1), (1, 0, 1), (1, 1, 1), (0, 1, 1)],
-    [(1, 0, 0), (1, 0, 1), (1, 1, 1), (1, 1, 0)],
-    [(0, 1, 0), (1, 1, 0), (1, 1, 1), (0, 1, 1)],
-]
-
-T = [(0, 0, -1), (-1, 0, 0), (0, -1, 0), (0, 0, 1), (1, 0, 0), (0, 1, 0)]
+SIDE_VERTEXES = {
+    (0, 0, -1): [(0, 0, 0), (1, 0, 0), (1, 1, 0), (0, 1, 0)],
+    (-1, 0, 0): [(0, 0, 0), (0, 0, 1), (0, 1, 1), (0, 1, 0)],
+    (0, -1, 0): [(0, 0, 0), (1, 0, 0), (1, 0, 1), (0, 0, 1)],
+    (0, 0, 1): [(0, 0, 1), (1, 0, 1), (1, 1, 1), (0, 1, 1)],
+    (1, 0, 0): [(1, 0, 0), (1, 0, 1), (1, 1, 1), (1, 1, 0)],
+    (0, 1, 0): [(0, 1, 0), (1, 1, 0), (1, 1, 1), (0, 1, 1)],
+}
 
 
 class Solution:
     def __init__(self, inp: Input):
-        self.cubes = [tuple([int(c) for c in line.split(",")]) for line in inp.get_lines()]
+        self.cubes = set([tuple([int(c) for c in line.split(",")]) for line in inp.get_lines()])
 
     def _get_alive(self):
-        s_cubes = set(self.cubes)
         alive = []
-        for i, a in enumerate(self.cubes):
-            for j, d in enumerate(T):
-                b = a[0] + d[0], a[1] + d[1], a[2] + d[2]
-                if b not in s_cubes:
-                    alive.append((i, j))
+        for cube in self.cubes:
+            for t in SIDE_VERTEXES:
+                if tuple(map(sum, zip(cube, t))) not in self.cubes:
+                    alive.append((cube, t))
         return alive
 
-    def _touch(self, x, y) -> Optional:
-        c1, c2 = self.cubes[x[0]], self.cubes[y[0]]
-        dx, dy, dz = abs(c1[0] - c2[0]), abs(c1[1] - c2[1]), abs(c1[2] - c2[2])
-        if max(dx, dy, dz) > 1 or dx + dy + dz > 2:  # don't edge touch
-            return
+    @staticmethod
+    def _get_sides_link(a, b) -> Optional:
+        cube_a, side_a = a
+        cube_b, side_b = b
 
-        t1, t2 = T[x[1]], T[y[1]]
-        if t1[0] + t2[0] == 0 and t1[1] + t2[1] == 0 and t1[2] + t2[2] == 0:  # opposite sides
-            return
+        dx, dy, dz = abs(cube_a[0] - cube_b[0]), abs(cube_a[1] - cube_b[1]), abs(cube_a[2] - cube_b[2])
+        if max(dx, dy, dz) > 1 or dx + dy + dz > 2:
+            return  # cubes a & b too far away to have a common edge
 
-        d1, d2 = D[x[1]], D[y[1]]
+        if side_a[0] + side_b[0] == side_a[1] + side_b[1] == side_a[2] + side_b[2] == 0:
+            return  # opposite sides of cubes can't be linked
 
-        p1 = set([(c1[0] + p[0], c1[1] + p[1], c1[2] + p[2]) for p in d1])
-        p2 = set([(c2[0] + p[0], c2[1] + p[1], c2[2] + p[2]) for p in d2])
-        common = p1.intersection(p2)
-        if len(common) == 2:
-            return tuple(sorted(common))
+        p1 = set([tuple(map(sum, zip(cube_a, p))) for p in SIDE_VERTEXES[side_a]])
+        p2 = set([tuple(map(sum, zip(cube_b, p))) for p in SIDE_VERTEXES[side_b]])
+        if len(common := p1 & p2) == 2:
+            return tuple(sorted(common))  # cubes touched within an edge by 2 common points
 
     def part_a(self):
         return len(self._get_alive())
 
     def part_b(self):
-        a = self._get_alive()
-        n = len(a)
-        links = [dict() for _ in range(n)]
-        for i in range(n - 1):
-            li = links[i]
-            for j in range(i + 1, n):
-                if c := self._touch(a[i], a[j]):
-                    lj = links[j]
-                    if c not in li or ((prev := li[c]) and prev[0] == a[i][0]):
-                        li[c] = a[j]
-                    if c not in lj or ((prev := lj[c]) and prev[0] == a[j][0]):
-                        lj[c] = a[i]
+        alive = self._get_alive()
+        links = dict()
+        for a, b in combinations(alive, 2):
+            if c := self._get_sides_link(a, b):
+                la = links.setdefault(a, {})
+                if c not in la or ((prev := la[c]) and prev[0] == a[0]):  # same cube has low priority in links for edge
+                    la[c] = b
+                lb = links.setdefault(b, {})
+                if c not in lb or ((prev := lb[c]) and prev[0] == b[0]):  # same cube has low priority in links for edge
+                    lb[c] = a
 
         graph = nx.Graph()
-        for i, l in enumerate(links):
+        for f, l in links.items():
             for c in l.values():
-                graph.add_edge(a[i], c)
+                graph.add_edge(f, c)
 
         components = list(nx.connected_components(graph))
 
