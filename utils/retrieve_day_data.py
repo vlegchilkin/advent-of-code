f2e9c0551ea755f3588349e470acf18b34dc4f42
Vitaly Legchilkin <vlegchilkin@gmail.com>
2022-12-21 14:44:51 +0100

    fix paths in the data retriever

--- utils/retrieve_day_data.py
+++ utils/retrieve_day_data.py
@@ -37,15 +37,20 @@ class Context:
         self.day = day
         self.session_cookie = os.getenv("SESSION")
         self.base_url = f"https://adventofcode.com/{year}/day/{day}"
-        self.sources_root = Path(__file__).parent.parent / f"aoc_{year}"
+
+        self.sources_root = Path(__file__).parent.parent / "aoc" / f"year_{year}"
+        if not self.sources_root.exists():
+            self.sources_root.mkdir(exist_ok=True, parents=True)
+            self.source("__init__.py").write("")
+
         self.resources_root = Path(__file__).parent.parent / "resources" / year / "day" / day
         self.resources_root.mkdir(exist_ok=True, parents=True)
 
     def resource(self, name) -> Resource:
         return Resource(self.resources_root / name)
 
-    def source(self) -> Resource:
-        return Resource(self.sources_root / f"day_{self.day}.py")
+    def source(self, filename) -> Resource:
+        return Resource(self.sources_root / filename)
 
     def request(self, action: str = ""):
         return requests.get(self.base_url + action, cookies={"session": self.session_cookie})
@@ -64,7 +69,7 @@ def slice_content(content: str, from_tags, to_tags) -> list[str]:
 if __name__ == "__main__":
     context = Context(*sys.argv[1:])
 
-    if not (source_file := context.source()).exists():
+    if not (source_file := context.source(f"day_{context.day}.py")).exists():
         source_file.copy(day_template.__file__)
 
     if not (input_file := context.resource("puzzle.in")).exists():
