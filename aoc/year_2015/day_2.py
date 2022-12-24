cf0aba8fdcab4dacd77e2d7404cad6aaf42459a2
Vitaly Legchilkin <vlegchilkin@gmail.com>
2022-12-24 22:30:13 +0100

    Year 2015 - Day 2

---
+++ aoc/year_2015/day_2.py
@@ -0,0 +1,32 @@
+import itertools
+
+import pytest
+
+from aoc import Input, get_puzzles, PuzzleData
+
+
+class Solution:
+    def __init__(self, inp: Input):
+        self.gifts = inp.get_lists("""{{ l | to_int }}x{{ w | to_int }}x{{ h | to_int }}""")
+
+    @staticmethod
+    def paper(gift):
+        result = sum([2 * c[0] * c[1] for c in itertools.combinations(gift, 2)])
+        s = sorted(gift)
+        return result + s[0] * s[1]
+
+    @staticmethod
+    def ribbon(gift):
+        s = sorted(gift)
+        return 2 * s[0] + 2 * s[1] + s[0] * s[1] * s[2]
+
+    def part_a(self):
+        return sum([self.paper(gift) for gift in self.gifts])
+
+    def part_b(self):
+        return sum([self.ribbon(gift) for gift in self.gifts])
+
+
+@pytest.mark.parametrize("pd", get_puzzles(), ids=str)
+def test_case(pd: PuzzleData):
+    pd.check_solution(Solution)
--- resources/2015/README.md
+++ resources/2015/README.md
@@ -24,6 +24,6 @@
 &nbsp;
 &nbsp;
 &nbsp;
-&nbsp;
+<a href='day/2'>Day 2: I Was Told There Would Be No Math</a>
 <a href='day/1'>Day 1: Not Quite Lisp</a>
 </pre>
