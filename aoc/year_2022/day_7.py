21226bebe7f8da4b445b716689443f3dc4a0d13d
Vitaly Legchilkin <vlegchilkin@gmail.com>
2022-12-26 11:55:36 +0100

    Year 2022 - Day 7 (cleanup)

--- aoc/year_2022/day_7.py
+++ aoc/year_2022/day_7.py
@@ -1,66 +1,81 @@
+from dataclasses import dataclass, field
+
 import pytest
 
 from aoc import Input, get_puzzles, PuzzleData
 
 
+@dataclass
+class Node:
+    parent: "Node" = None
+    children: dict[str, "Node"] = field(default_factory=lambda: dict())
+    files_size: int = 0
+    total: int = 0
+
+
 class Solution:
     def __init__(self, inp: Input):
         input_iter = inp.get_iter()
+        self.root = node = Node()
 
-        self.root = node = {"children": {}}
         line = next(input_iter)
         while line:
             if line.startswith("$ cd"):
-                if (name := line[5:]) == "..":
-                    node = node["parent"]
-                else:
-                    if name not in node["children"]:
-                        child = {"parent": node, "children": {}}
-                        node["children"][name] = child
-                        node = child
-                    else:
-                        node = node["children"][name]
+                node = self._cd(line, node)
                 line = next(input_iter, None)
             elif line.startswith("$ ls"):
-                total = 0
-                while (line := next(input_iter, None)) and not line.startswith("$"):
-                    if not line.startswith("dir "):
-                        size, _ = line.split(" ")
-                        total += int(size)
-                node["files_size"] = total
+                line = self._ls(input_iter, node)
             else:
                 raise ValueError(f"Wrong Input: {line}")
 
         self._count_total(self.root)
 
-    def _count_total(self, node) -> int:
-        result = node["files_size"] if "files_size" in node else 0
+    @staticmethod
+    def _ls(input_iter, node):
+        while (line := next(input_iter, None)) and not line.startswith("$"):
+            if not line.startswith("dir "):
+                size, _ = line.split(" ")
+                node.files_size += int(size)
+        return line
 
-        for v in node["children"].values():
-            result += self._count_total(v)
+    @staticmethod
+    def _cd(line, node):
+        if (name := line[5:]) == "..":
+            return node.parent
 
-        node["total"] = result
+        if name not in node.children:
+            node.children[name] = Node(parent=node)
+
+        return node.children[name]
+
+    def _count_total(self, node: Node) -> int:
+        result = node.files_size
+
+        for child_node in node.children.values():
+            result += self._count_total(child_node)
+
+        node.total = result
         return result
 
     def part_a(self):
         return self._part_a_sol(self.root)
 
-    def _part_a_sol(self, node) -> int:
-        result = t if (t := node["total"]) < 100000 else 0
+    def _part_a_sol(self, node: Node) -> int:
+        result = t if (t := node.total) < 100000 else 0
 
-        for v in node["children"].values():
+        for v in node.children.values():
             result += self._part_a_sol(v)
 
         return result
 
     def part_b(self):
-        need_space = 30000000 - (70000000 - self.root["total"])
+        need_space = 30000000 - (70000000 - self.root.total)
         return self._part_b_sol(self.root, need_space)
 
-    def _part_b_sol(self, node, goal) -> int:
-        best = t if (t := node["total"]) >= goal else None
+    def _part_b_sol(self, node: Node, goal) -> int:
+        best = t if (t := node.total) >= goal else None
 
-        for v in node["children"].values():
+        for v in node.children.values():
             if (_best := self._part_b_sol(v, goal)) and (not best or _best < best):
                 best = _best
 
