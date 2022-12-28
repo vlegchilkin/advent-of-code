446a97caac77392684c2c78da14d2b98a7d91735
Vitaly Legchilkin <vlegchilkin@gmail.com>
2022-12-28 20:20:18 +0100

    Year 2015 - Day 19

--- aoc/algo.py
+++ aoc/algo.py
@@ -35,3 +35,43 @@ def cyk(rules, text, rule_weights=None, backtrack=False) -> [dict, Optional[dict
                                     paths[i][j][lhs] = [lhs, paths[i][k].get(rhs[0]), paths[k + 1][j].get(rhs[1])]
 
     return weights[0][n - 1], paths[0][n - 1] if paths else None
+
+
+def make_cnf(rules) -> dict:
+    doubles = {v: rep for rep, val in rules.items() for v in val if len(v) == 2}
+    mapping = {}
+
+    def get_mapped(st) -> str:
+        if st not in mapping:
+            mapping[st] = chr(len(mapping) + ord("А"))
+        return mapping[st]
+
+    def split(st):
+        nonlocal doubles
+
+        if len(st) > 2:
+            x = st
+            while len(x) != 2:
+                if len(x) > 3:
+                    x = split(x[:2]) + split(x[2:])
+                else:
+                    x = x[0] + split(x[1:])
+            return x
+
+        if st not in doubles:
+            x = get_mapped(st)
+            doubles[st] = x
+            rules[x] = [st]
+
+        return doubles[st]
+
+    for rep in list(rules):
+        new_val = []
+        for v in rules[rep]:
+            if len(v) <= 2:
+                new_val.append(v)
+            else:
+                new_val.append(split(v))
+        rules[rep] = new_val
+
+    return rules
--- aoc/year_2015/day_19.py
+++ aoc/year_2015/day_19.py
@@ -10,44 +10,39 @@ class Solution:
     def __init__(self, inp: Input):
         inp_iter = inp.get_iter()
         self.mapping = {}
-        self.replacements = {}
+        self.replacements = self._parse_replacements(inp_iter)
+        self.molecule = self._normalize_elements([next(inp_iter)])[0]
+
+    def _mapped(self, element) -> str:
+        if element not in self.mapping:
+            self.mapping[element] = chr(len(self.mapping) + ord("A"))
+        return self.mapping[element]
+
+    def _parse_replacements(self, inp_iter) -> dict:
+        replacements = {}
         while line := next(inp_iter).strip():
-            rule = self._normalize(line.split(" => "))
-            self.replacements.setdefault(rule[0], []).append(rule[1])
+            rule = self._normalize_elements(line.split(" => "))
+            replacements.setdefault(rule[0], []).append(rule[1])
 
         for m, v in self.mapping.items():
-            if v not in self.replacements:
-                self.replacements[v] = []
+            if v not in replacements:
+                replacements[v] = []
+        return replacements
 
-        self.molecule = self._normalize([next(inp_iter)])[0]
-
-    def get_mapped(self, st) -> str:
-        if not st:
-            return ""
-        if st not in self.mapping:
-            x = len(self.mapping)
-            if x < 26:
-                val = chr(x + ord("A"))
-            else:
-                val = chr((x - 26) + ord("0"))
-            self.mapping[st] = val
-        return self.mapping[st]
-
-    def _normalize(self, strs: list[str]) -> list[str]:
+    def _normalize_elements(self, strs: list[str]) -> list[str]:
         result = []
         for s in strs:
             r = ""
-            last = None
-            for c in s:
-                if c in string.ascii_uppercase:
-                    r += self.get_mapped(last)
-                    last = c
+            element = None
+            for c in s + "\n":
+                if c in string.ascii_uppercase + "\n":
+                    r += self._mapped(element) if element else ""
+                    element = c
                 else:
                     if c == "e":
-                        last = "$"
+                        element = "$"
                     else:
-                        last += c
-            r += self.get_mapped(last)
+                        element += c
             result.append(r)
         return result
 
@@ -63,42 +58,16 @@ class Solution:
         return len(all_possible)
 
     def part_b(self):
-        base_rules = set(self.replacements)
         rules = copy.deepcopy(self.replacements)
 
         for rep, val in rules.items():
             if rep != self.mapping["$"]:
                 rules[rep].append(rep.lower())
 
-        doubles = {v: rep for rep, val in rules.items() for v in val if len(v) == 2}
-
-        def split(st):
-            nonlocal doubles
-
-            if len(st) > 2:
-                x = st
-                while len(x) != 2:
-                    x = split(x[:2]) + (split(x[2:]) if len(x) > 3 else x[2])
-                return x
-
-            if st not in doubles:
-                x = self.get_mapped(st)
-                doubles[st] = x
-                rules[x] = [st]
-
-            return doubles[st]
-
-        for rep in list(rules):
-            new_val = []
-            for v in rules[rep]:
-                if len(v) <= 2:
-                    new_val.append(v)
-                else:
-                    new_val.append(split(v))
-            rules[rep] = new_val
+        rules = algo.make_cnf(rules)
 
         molecule = self.molecule.lower()
-        rule_weights = {r: 1 for r in base_rules}
+        rule_weights = {r: 1 for r in set(self.replacements)}
         weights, _ = algo.cyk(rules, molecule, rule_weights)
         return weights[self.mapping["$"]]
 
