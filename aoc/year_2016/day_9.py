4380655a7dc51b46891b99ea0ff5f14cc918db9f
Vitaly Legchilkin <vlegchilkin@gmail.com>
2023-01-10 13:50:16 +0100

    Year 2016 - Day 9

---
+++ aoc/year_2016/day_9.py
@@ -0,0 +1,36 @@
+import itertools
+
+import pytest
+
+from aoc import Input, get_puzzles, PuzzleData, ISolution
+
+
+class Solution(ISolution):
+    """2016/9: Explosives in Cyberspace"""
+
+    def __init__(self, inp: Input):
+        self.line = inp.get_lines()[0]
+
+    def find_length(self, s, format_version) -> int:
+        it = iter(s)
+        result = 0
+        while (ch := next(it, None)) is not None:
+            if ch != "(":
+                result += 1
+            else:
+                marker = "".join(itertools.takewhile(lambda c: c if c != ")" else None, it))
+                length, count = map(int, marker.split("x"))
+                subs = "".join(next(it) for _ in range(length))
+                result += count * (len(subs) if format_version == 1 else self.find_length(subs, format_version))
+        return result
+
+    def part_a(self):
+        return self.find_length(self.line, 1)
+
+    def part_b(self):
+        return self.find_length(self.line, 2)
+
+
+@pytest.mark.parametrize("pd", get_puzzles(), ids=str)
+def test_case(pd: PuzzleData):
+    pd.check_solution(Solution)
--- resources/2016/README.md
+++ resources/2016/README.md
@@ -22,7 +22,7 @@
 &nbsp;
 &nbsp;
 &nbsp;
-&nbsp;
+<a href='day/9'>Day 9: Explosives in Cyberspace</a>
 <a href='day/8'>Day 8: Two-Factor Authentication</a>
 <a href='day/7'>Day 7: Internet Protocol Version 7</a>
 <a href='day/6'>Day 6: Signals and Noise</a>
