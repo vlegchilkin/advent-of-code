f8a3e02c1e70c158c07982e6632a4e86a8ba4dbe
Vitaly Legchilkin <vlegchilkin@gmail.com>
2023-01-12 19:26:32 +0100

    Year 2016 - Day 19

---
+++ aoc/year_2016/day_19.py
@@ -0,0 +1,39 @@
+import pytest
+from llist import dllist
+
+from aoc import Input, get_puzzles, PuzzleData, ISolution
+
+
+class Solution(ISolution):
+    """2016/19: An Elephant Named Joseph"""
+
+    def __init__(self, inp: Input):
+        self.n = int(inp.get_lines()[0])
+
+    def part_a(self):
+        d = dllist([i for i in range(1, self.n + 1)])
+        node = d.first
+        while d.first.next:
+            d.remove(node.next or d.first)
+            node = node.next or d.first
+        return node.value
+
+    def part_b(self):
+        d = dllist([i for i in range(1, self.n + 1)])
+        node, _len, index = d.first, len(d), 0
+        while _len > 1:
+            idx = (index + (_len // 2)) % _len
+            del d[idx]
+            _len -= 1
+            if node.next:
+                node = node.next
+                index += 1 if idx > index else 0
+            else:
+                node = d.first
+                index = 0
+        return d.first.value
+
+
+@pytest.mark.parametrize("pd", get_puzzles(), ids=str)
+def test_case(pd: PuzzleData):
+    pd.check_solution(Solution)
--- resources/2016/README.md
+++ resources/2016/README.md
@@ -12,7 +12,7 @@
 &nbsp;
 &nbsp;
 &nbsp;
-&nbsp;
+<a href='day/19'>Day 19: An Elephant Named Joseph</a>
 <a href='day/18'>Day 18: Like a Rogue</a>
 <a href='day/17'>Day 17: Two Steps Forward</a>
 <a href='day/16'>Day 16: Dragon Checksum</a>
