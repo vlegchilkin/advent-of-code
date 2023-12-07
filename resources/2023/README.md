f9972cc8001a58a63c59f7cea561179c2777a382
Vitaly Legchilkin <vlegchilkin@gmail.com>
2023-12-07 07:52:51 +0100

    Year 2023 Day 7

--- resources/2023/README.md
+++ resources/2023/README.md
@@ -12,14 +12,14 @@
 &nbsp;
 &nbsp;
 &nbsp;
-&nbsp;
-&nbsp;
+<a href='day/7'>Day 7: Camel Cards</a>
 <a href='day/6'>Day 6: Wait For It</a>
 <a href='day/5'>Day 5: If You Give A Seed A Fertilizer</a>
 <a href='day/4'>Day 4: Scratchcards</a>
 <a href='day/3'>Day 3: Gear Ratios</a>
 <a href='day/2'>Day 2: Cube Conundrum</a>
 <a href='day/1'>Day 1: Trebuchet?!</a>
+&nbsp;
 &nbsp;
 &nbsp;
 &nbsp;
---
+++ solutions/kotlin/src/org/vlegchilkin/aoc/year2023/Year2023Day7.kt
@@ -0,0 +1,61 @@
+package org.vlegchilkin.aoc.year2023
+
+import org.vlegchilkin.aoc.*
+
+
+/**
+ * 2023/7: Camel Cards
+ */
+class Year2023Day7(input: String) : Solution {
+  private val hands = input.toList {
+    val data = it.split(' ')
+    Hand(data[0], data[1].toInt())
+  }
+
+  data class Hand(val cards: String, val bet: Int)
+  enum class HandType {
+    FIVE, FOUR, FULL_HOUSE, THREE, TWO_PAIR, ONE_PAIR, HIGH_CARD;
+
+    companion object {
+      fun of(hand: String): HandType {
+        val groups = hand.groupingBy { it }.eachCount().values.sortedDescending()
+        return when (groups.size) {
+          1 -> FIVE
+          2 -> if (groups[0] == 4) FOUR else FULL_HOUSE
+          3 -> if (groups[0] == 3) THREE else TWO_PAIR
+          4 -> ONE_PAIR
+          else -> HIGH_CARD
+        }
+      }
+    }
+  }
+
+  private fun totalWinnings(deck: String, typeOf: (String) -> HandType): Int {
+    fun powerOf(hand: String): Int {
+      val type = typeOf(hand)
+      val cp = hand.map { deck.indexOf(it) }
+      return (type.ordinal shl 20) + (cp[0] shl 16) + (cp[1] shl 12) + (cp[2] shl 8) + (cp[3] shl 4) + cp[4]
+    }
+
+    val handPowers = hands.map { it to powerOf(it.cards) }
+    return handPowers
+      .sortedWith(compareBy { it.second }).asReversed()
+      .mapIndexed { i, (hand, _) -> hand.bet * (i + 1) }.sum()
+  }
+
+
+  override fun partA(): Any {
+    return totalWinnings("AKQJT98765432") { hand ->
+      HandType.of(hand)
+    }
+  }
+
+  override fun partB(): Any {
+    val deck = "AKQT98765432J"
+    return totalWinnings(deck) { hand ->
+      deck.minOf { HandType.of(hand.replace('J', it)) }
+    }
+  }
+
+  companion object : Test(2023, 7, { Year2023Day7(it) })
+}
