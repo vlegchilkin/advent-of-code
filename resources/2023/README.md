ed42713d4c2250dab5688045d4b68f95e02f15d1
Vitaly Legchilkin <vlegchilkin@gmail.com>
2023-12-11 08:47:49 +0100

    Year 2023 Day 11

--- resources/2023/README.md
+++ resources/2023/README.md
@@ -6,8 +6,7 @@
 &nbsp;
 &nbsp;
 &nbsp;
-&nbsp;
-&nbsp;
+<a href='day/11'>Day 11: Cosmic Expansion</a>
 <a href='day/10'>Day 10: Pipe Maze</a>
 <a href='day/9'>Day 9: Mirage Maintenance</a>
 <a href='day/8'>Day 8: Haunted Wasteland</a>
@@ -18,6 +17,7 @@
 <a href='day/3'>Day 3: Gear Ratios</a>
 <a href='day/2'>Day 2: Cube Conundrum</a>
 <a href='day/1'>Day 1: Trebuchet?!</a>
+&nbsp;
 &nbsp;
 &nbsp;
 &nbsp;
---
+++ solutions/kotlin/src/org/vlegchilkin/aoc/year2023/Year2023Day11.kt
@@ -0,0 +1,44 @@
+package org.vlegchilkin.aoc.year2023
+
+import org.vlegchilkin.aoc.*
+import kotlin.math.absoluteValue
+
+
+/**
+ * 2023/11: Cosmic Expansion
+ */
+class Year2023Day11(input: String) : Solution {
+  private val space = input.toCSpace { it.takeIf { it != '.' } }
+
+  private fun getExpansion(gravitationEffect: Int): Pair<List<Long>, List<Long>> {
+    var total = 0L
+    val rExp = space.rows.map { r ->
+      total += (space.cols.all { c -> (r to c) !in space }).toInt() * (gravitationEffect - 1)
+      total
+    }
+    total = 0L
+    val cExp = space.cols.map { c ->
+      total += (space.rows.all { r -> (r to c) !in space }).toInt() * (gravitationEffect - 1)
+      total
+    }
+    return rExp to cExp
+  }
+
+  private fun sumOfDistances(gravitationEffect: Int): Long {
+    val (rExp, cExp) = getExpansion(gravitationEffect)
+    val distances = space.keys.combinations(2).map { (a, b) ->
+      (a manhattanTo b) + (rExp[a.first] - rExp[b.first]).absoluteValue + (cExp[a.second] - cExp[b.second]).absoluteValue
+    }
+    return distances.sum()
+  }
+
+  override fun partA(): Long {
+    return sumOfDistances(2)
+  }
+
+  override fun partB(): Any {
+    return sumOfDistances(1000000)
+  }
+
+  companion object : Test(2023, 11, { Year2023Day11(it) })
+}
