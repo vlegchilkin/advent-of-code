27dd2b6c9698102ac8e7ac951d9c397ada08e232
Vitaly Legchilkin <vlegchilkin@gmail.com>
2023-12-19 11:19:08 +0100

    Year 2023 Day 19 * clean up

--- solutions/kotlin/src/org/vlegchilkin/aoc/year2023/Year2023Day19.kt
+++ solutions/kotlin/src/org/vlegchilkin/aoc/year2023/Year2023Day19.kt
@@ -7,26 +7,22 @@ import org.vlegchilkin.aoc.*
  * 2023/19: Aplenty
  */
 class Year2023Day19(input: String) : Solution {
-  private val workflows: Map<String, List<Rule>>
+  private val workflows: Map<String, Workflow>
   private val parts: List<Map<Char, Int>>
 
-  data class Rule(val field: Char? = null, val sign: Char? = null, val value: Int? = null, val action: String)
+  data class Rule(val field: Char, val sign: Char, val value: Int, val action: String)
+  data class Workflow(val rules: List<Rule>, val defaultAction: String)
 
   init {
     val (wfData, pData) = input.toList("\n\n") { it.split("\n") }
     workflows = wfData.associate { d ->
       val name = d.substringBefore("{")
       val groups = d.removeSurrounding("$name{", "}").split(',')
-      val rules = groups.map { r ->
-        if (r.contains(":")) {
-          val (a, b) = r.split(':')
-          Rule(a[0], a[1], a.substring(2).toInt(), b)
-        }
-        else {
-          Rule(action = r)
-        }
+      val rules = groups.dropLast(1).map { r ->
+        val (a, b) = r.split(':')
+        Rule(a[0], a[1], a.substring(2).toInt(), b)
       }
-      name to rules
+      name to Workflow(rules, groups.last())
     }
     parts = pData.map { d ->
       d.removeSurrounding("{", "}").split(',').associate {
@@ -37,33 +33,29 @@ class Year2023Day19(input: String) : Solution {
   }
 
   private fun dfs(action: String, available: Map<Char, IntRange>): Long {
+    if (available.values.any { it.isEmpty() }) return 0L
     when (action) {
       "R" -> return 0L
       "A" -> return available.values.fold(1L) { acc, c -> acc * (c.last - c.first + 1) }
     }
+    val workflow = workflows[action]!!
+    val ranges = available.toMutableMap()
 
     var result = 0L
-    val ranges = available.toMutableMap()
-    for (rule in workflows[action]!!) {
-      if (rule.value == null) {
-        result += dfs(rule.action, ranges)
-        break
-      }
-      val r = ranges[rule.field!!]!!
+    for (rule in workflow.rules) {
+      val range = ranges[rule.field]!!
       val (take, remains) = when (rule.sign) {
-        '<' -> r.first..minOf(rule.value - 1, r.last) to rule.value..r.last
-        '>' -> maxOf(rule.value + 1, r.first)..r.last to r.first..rule.value
+        '<' -> range.first..minOf(rule.value - 1, range.last) to rule.value..range.last
+        '>' -> maxOf(rule.value + 1, range.first)..range.last to range.first..rule.value
         else -> error("Illegal sign")
       }
-      if (take.isEmpty()) continue
-
-      ranges[rule.field] = take
-      result += dfs(rule.action, ranges)
-
-      if (remains.isEmpty()) break
-      ranges[rule.field] = remains
+      if (!take.isEmpty()) {
+        ranges[rule.field] = take
+        result += dfs(rule.action, ranges)
+        ranges[rule.field] = remains
+      }
     }
-
+    result += dfs(workflow.defaultAction, ranges)
     return result
   }
 
