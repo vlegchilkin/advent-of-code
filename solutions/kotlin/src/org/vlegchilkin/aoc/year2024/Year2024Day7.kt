b2b30e58ed10f121fee1db05cdb7d9c977fa1cb9
Vitaly Legchilkin <vlegchilkin@gmail.com>
2024-12-07 11:00:19 +0100

    2024 day 7 (Kotlin)

--- solutions/kotlin/src/org/vlegchilkin/aoc/year2024/Year2024Day7.kt
+++ solutions/kotlin/src/org/vlegchilkin/aoc/year2024/Year2024Day7.kt
@@ -3,23 +3,16 @@ package org.vlegchilkin.aoc.year2024
 import org.vlegchilkin.aoc.*
 
 data class Equation(val total: Long, val elements: List<Long>) {
-  fun isPossible(allowedOperators: String): Boolean {
+  fun isPossible(vararg operators: (Long, Long) -> Long): Boolean {
 
     fun dfs(idx: Int = 0, sum: Long = 0): Boolean {
       if (idx == elements.size) return sum == total
       if (sum > total) return false
       if (idx == 0) return dfs(1, elements[0])
 
-      for (operator in allowedOperators) {
-        val nSum = when (operator) {
-          '+' -> sum + elements[idx]
-          '*' -> sum * elements[idx]
-          '|' -> "$sum${elements[idx]}".toLong()
-          else -> error("Invalid operator: $operator")
-        }
-        if (dfs(idx + 1, nSum)) return true
+      return operators.any { operator ->
+        dfs(idx + 1, operator(sum, elements[idx]))
       }
-      return false
     }
 
     return dfs()
@@ -36,11 +29,26 @@ class Year2024Day7(input: String) : Solution {
   }
 
   override fun partA(): Any {
-    return equations.filter { it.isPossible("+*") }.sumOf { it.total }
+    val possible = equations.filter {
+      it.isPossible(
+        Long::plus,
+        Long::times
+      )
+    }
+
+    return possible.sumOf { it.total }
   }
 
   override fun partB(): Any {
-    return equations.filter { it.isPossible("+*|") }.sumOf { it.total }
+    val possible = equations.filter {
+      it.isPossible(
+        Long::plus,
+        Long::times,
+        { a, b -> "$a$b".toLong() }
+      )
+    }
+
+    return possible.sumOf { it.total }
   }
 
   companion object : Test(2024, 7, { Year2024Day7(it) })
