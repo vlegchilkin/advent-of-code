e429c585c7dcc353b1e148729364c2af22c5a2ac
Vitaly Legchilkin <vlegchilkin@gmail.com>
2024-12-08 11:19:15 +0100

    2024 day 7 (Kotlin)

--- solutions/kotlin/src/org/vlegchilkin/aoc/year2024/Year2024Day8.kt
+++ solutions/kotlin/src/org/vlegchilkin/aoc/year2024/Year2024Day8.kt
@@ -20,24 +20,19 @@ class Year2024Day8(input: String) : Solution {
         .forEach { acc.add(it) }
       acc
     }
-
     return antinodes.size
   }
 
   override fun partB(): Any {
-    fun MutableSet<C>.addTNodes(pos: C, delta: C) {
-      val inRangeNodes = generateSequence(pos) { nPos ->
-        (nPos + delta).takeIf { space.isBelongs(it) }
-      }
-      this.addAll(inRangeNodes)
+    fun generateTNodes(pos: C, delta: C) = generateSequence(pos) { nPos ->
+      (nPos + delta).takeIf { space.isBelongs(it) }
     }
 
     val antinodes = space.pairs().fold(mutableSetOf<C>()) { acc, (a, b) ->
-      acc.addTNodes(a, a-b)
-      acc.addTNodes(b, b-a)
+      acc.addAll(generateTNodes(a, a - b))
+      acc.addAll(generateTNodes(b, b - a))
       acc
     }
-
     return antinodes.size
   }
 
