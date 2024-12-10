13c7dac536142a6dd088ab32e5e6b928e1aa2f9d
Vitaly Legchilkin <vlegchilkin@gmail.com>
2024-12-10 11:53:05 +0100

    2024 day 10 (Kotlin)

--- solutions/kotlin/src/org/vlegchilkin/aoc/year2024/Year2024Day10.kt
+++ solutions/kotlin/src/org/vlegchilkin/aoc/year2024/Year2024Day10.kt
@@ -11,36 +11,26 @@ const val FINISH: Int = 9
 class Year2024Day10(input: String) : Solution {
   private val map = input.toCSpace { it.digitToInt() }
 
-  override fun partA(): Any {
-    val routes = map.transform { pos, height -> if (height == START) mutableSetOf(pos) else mutableSetOf() }
-
-    for (height in START + 1..FINISH) {
-      map.filterValues { it == height }.keys.forEach { pos ->
-        val lowerPositions = map.links(pos, Direction.borders()) { map[it] == height - 1 }
-        routes[pos]?.let { posRoutes ->
-          lowerPositions.mapNotNull { routes[it] }.forEach { lowerRoutes -> posRoutes.addAll(lowerRoutes) }
-        }
-      }
-    }
-
-    val finishPositions = map.filterValues { it == FINISH }.keys
-    val total = finishPositions.mapNotNull { routes[it] }.sumOf { it.size }
-    return total
-  }
-
-  override fun partB(): Any {
+  override fun partAB(): Pair<Int, Int> {
+    val routesCounter = map.transform { pos, height -> if (height == START) mutableSetOf(pos) else mutableSetOf() }
     val pathsCounter = map.transform { _, high -> if (high == START) 1 else 0 }
 
-    for (height in START + 1..FINISH) {
-      map.filterValues { it == height }.keys.forEach { pos ->
-        val lowerPositions = map.links(pos, Direction.borders()) { map[it] == height - 1 }
-        pathsCounter[pos] = lowerPositions.mapNotNull { pathsCounter[it] }.sum()
+    val heightPositions = map.entries.groupBy({ it.value }, { it.key }).toSortedMap()
+
+    heightPositions.entries.drop(1).forEach { (height, positions) ->
+      for (pos in positions) {
+        val stepDownNeighbours = map.links(pos, Direction.borders()) { map[it] == height - 1 }
+        routesCounter[pos]?.let { posRoutes ->
+          stepDownNeighbours.mapNotNull { routesCounter[it] }.forEach { lowerRoutes -> posRoutes.addAll(lowerRoutes) }
+        }
+        pathsCounter[pos] = stepDownNeighbours.mapNotNull { pathsCounter[it] }.sum()
       }
     }
 
-    val finishPositions = map.filterValues { it == FINISH }.keys
-    val total = finishPositions.mapNotNull { pathsCounter[it] }.sum()
-    return total
+    val finishPositions = heightPositions[FINISH] ?: emptyList()
+    val differentRoutes = finishPositions.mapNotNull { routesCounter[it] }.sumOf { it.size }
+    val differentPaths = finishPositions.mapNotNull { pathsCounter[it] }.sum()
+    return differentRoutes to differentPaths
   }
 
   companion object : Test(2024, 10, { Year2024Day10(it) })
