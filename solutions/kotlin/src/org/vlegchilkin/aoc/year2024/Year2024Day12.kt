018f6ec864303f4055c7f03124dbee68e1fae6a2
Vitaly Legchilkin <vlegchilkin@gmail.com>
2024-12-12 13:50:23 +0100

    2024 day 12 (Kotlin)

--- solutions/kotlin/src/org/vlegchilkin/aoc/year2024/Year2024Day12.kt
+++ solutions/kotlin/src/org/vlegchilkin/aoc/year2024/Year2024Day12.kt
@@ -10,36 +10,36 @@ import org.vlegchilkin.aoc.Direction.*
 class Year2024Day12(input: String) : Solution {
   private val garden = input.toCSpace { it }
 
+  enum class Corner(private val side: Direction, private val outer: Direction, private val inner: Direction) {
+    TOP_LEFT(N, W, NW), TOP_RIGHT(E, N, NE), BOTTOM_RIGHT(S, E, SE), BOTTOM_LEFT(W, S, SW);
+
+    fun check(pos: C, area: Set<C>) = ((pos + side) !in area) and (((pos + outer) !in area) or ((pos + inner) in area))
+  }
+
   override fun partAB(): Pair<Int, Int> {
-    val regions = CSpace<Int>(garden.rows, garden.cols, mutableMapOf())
-    var idCounter = 0
-    garden.keys.asSequence().filter { pos -> pos !in regions }.forEach { pos ->
-      regions[pos] = ++idCounter
-      regions.fillByTemplate(pos, garden)
+    val regionsMap = CSpace<String>(garden.rows, garden.cols, mutableMapOf())
+    val idCounter = mutableMapOf<Char, Int>()
+    garden.entries.asSequence().filter { (pos) -> pos !in regionsMap }.forEach { (pos, c) ->
+      val counter = idCounter.compute(c) { _, counter -> counter?.plus(1) ?: 0 }
+      regionsMap[pos] = "$c.$counter"
+      regionsMap.fillByTemplate(pos, garden)
     }
+    val regionPositions = regionsMap.entries.groupBy({ it.value }, { it.key }).mapValues { it.value.toSet() }
 
-    val regionPositions = regions.entries.groupBy({ it.value }, { it.key }).mapValues { it.value.toSet() }
+    data class Region(val id: String, val area: Int, val perimeter: Int, val sides: Int)
 
-    val regionPerimeter = regionPositions.mapValues { (_, positions) ->
-      positions.sumOf { pos ->
+    val regions = regionPositions.entries.map { (id, positions) ->
+      val perimeter = positions.sumOf { pos ->
         Direction.borders().count { (pos + it) !in positions }
       }
-    }
-
-    data class Corner(val side: Direction, val outer: Direction, val inner: Direction)
-
-    val possibleCorners = listOf(Corner(N, W, NW), Corner(E, N, NE), Corner(S, E, SE), Corner(W, S, SW))
-
-    val regionSides = regionPositions.mapValues { (_, positions) ->
-      positions.sumOf { pos ->
-        possibleCorners.count { (side, outer, inner) ->
-          ((pos + side) !in positions) and (((pos + outer) !in positions) or ((pos + inner) in positions))
-        }
+      val sides = positions.sumOf { pos ->
+        Corner.entries.count { it.check(pos, positions) }
       }
+      Region(id, positions.size, perimeter, sides)
     }
 
-    val partA = regionPositions.entries.sumOf { (id, region) -> region.size * regionPerimeter.getOrDefault(id, 0) }
-    val partB = regionPositions.entries.sumOf { (id, region) -> region.size * regionSides.getOrDefault(id, 0) }
+    val partA = regions.sumOf { it.area * it.perimeter }
+    val partB = regions.sumOf { it.area * it.sides }
     return partA to partB
   }
 
