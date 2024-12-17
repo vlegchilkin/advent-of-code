53038a2a6a46ebbab032a4758802701394c8b36b
Vitaly Legchilkin <vlegchilkin@gmail.com>
2024-12-17 12:16:17 +0100

    2024 day 17 (Kotlin)

--- solutions/kotlin/src/org/vlegchilkin/aoc/year2024/Year2024Day17.kt
+++ solutions/kotlin/src/org/vlegchilkin/aoc/year2024/Year2024Day17.kt
@@ -4,7 +4,7 @@ import org.vlegchilkin.aoc.*
 import org.vlegchilkin.aoc.year2024.ChronoSpatialComputer.OpCode.*
 
 
-class ChronoSpatialComputer(private val memory: List<Int>) {
+class ChronoSpatialComputer(private val program: List<Int>) {
   enum class OpCode(val value: Int) {
     ADV(0), BXL(1), BST(2), JNZ(3), BXC(4), OUT(5), BDV(6), CDV(7);
 
@@ -13,34 +13,35 @@ class ChronoSpatialComputer(private val memory: List<Int>) {
     }
   }
 
-  fun run(regInitValues: List<Long>): List<Int> {
-    val registers = regInitValues.toLongArray()
+  fun run(registerInitValues: List<Long>): List<Int> {
+    val registers = registerInitValues.toLongArray()
     val output: MutableList<Int> = mutableListOf()
     var pointer = 0
 
-    fun nextLiteralOperand() = memory[pointer++].toLong()
-    fun nextComboOperand() = when (val operand = nextLiteralOperand()) {
-      in 0..3 -> operand
-      in 4..6 -> registers[operand.toInt() - 4]
+    fun nextLiteral(): Int = program[pointer++]
+    fun nextComboOperand(): Long = when (val operand = nextLiteral()) {
+      in 0..3 -> operand.toLong()
+      in 4..6 -> registers[operand - 4]
       else -> error("Non-supported combo operand $operand")
     }
 
-    while (pointer <= memory.lastIndex) {
-      when (OpCode.of(memory[pointer++])) {
+    while (pointer <= program.lastIndex) {
+      when (OpCode.of(nextLiteral())) {
         ADV -> {
           registers[0] = registers[0] shr nextComboOperand().toInt()
         }
         BXL -> {
-          registers[1] = registers[1] xor nextLiteralOperand()
+          registers[1] = registers[1] xor nextLiteral().toLong()
         }
         BST -> {
           registers[1] = nextComboOperand() % 8
         }
         JNZ -> {
-          if (registers[0] != 0L) pointer = nextLiteralOperand().toInt() else pointer++
+          val reference = nextLiteral()
+          if (registers[0] != 0L) pointer = reference
         }
         BXC -> {
-          pointer += 1
+          nextLiteral()
           registers[1] = registers[1] xor registers[2]
         }
         OUT -> {
@@ -62,18 +63,17 @@ class ChronoSpatialComputer(private val memory: List<Int>) {
  * 2024/17: Chronospatial Computer
  */
 class Year2024Day17(input: String) : Solution {
-  private val registers: List<Int>
+  private val registerInitValues: List<Long>
   private val program: List<Int>
 
   init {
     val (registers, program) = input.toList("\n\n") { it.toIntList() }
-    this.registers = registers
+    this.registerInitValues = registers.map { it.toLong() }
     this.program = program
   }
 
   override fun partA(): String {
-    val computer = ChronoSpatialComputer(program)
-    val output = computer.run(registers.map { it.toLong() })
+    val output = ChronoSpatialComputer(program).run(registerInitValues)
     return output.joinToString(separator = ",")
   }
 
@@ -83,13 +83,12 @@ class Year2024Day17(input: String) : Solution {
     fun dfs(prefix: Long, len: Int): Long? {
       if (len == program.size) return prefix
       val expected = program.takeLast(len + 1)
-      IntRange(0, 7).map { (prefix shl 3) + it }.forEach { regA ->
-        if (computer.run(listOf(regA, 0, 0)) == expected) {
-          val solution = dfs(regA, len + 1)
-          if (solution != null) return solution
-        }
+      val possibleA = IntRange(0, 7).map { (prefix shl 3) + it }
+      val solution = possibleA.firstNotNullOfOrNull { regA ->
+        val output = computer.run(listOf(regA, 0, 0))
+        if (output == expected) dfs(regA, len + 1) else null
       }
-      return null
+      return solution
     }
 
     val minA = dfs(0L, 0) ?: error("Part b has no solution")
