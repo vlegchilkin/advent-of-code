76535bc310bd86f5d72aabc22ffc2bd093557044
Vitaly Legchilkin <vlegchilkin@gmail.com>
2024-12-24 17:20:54 +0100

    2024 day 24 (Kotlin)

--- solutions/kotlin/src/org/vlegchilkin/aoc/year2024/Year2024Day24.kt
+++ solutions/kotlin/src/org/vlegchilkin/aoc/year2024/Year2024Day24.kt
@@ -2,32 +2,28 @@ package org.vlegchilkin.aoc.year2024
 
 import org.vlegchilkin.aoc.*
 
-enum class Command(val op: (Int, Int) -> Int) {
-  AND(Int::and),
-  XOR(Int::xor),
-  OR(Int::or)
-}
+enum class Logic(val op: (Int, Int) -> Int) { AND(Int::and), XOR(Int::xor), OR(Int::or) }
 
-data class Instruction(val cmd: Command, val in1: String, val in2: String, val out: String)
+data class Element(val logic: Logic, val in1: String, val in2: String, val out: String)
 
 /**
  * 2024/24: Crossed Wires
  */
 class Year2024Day24(input: String) : Solution {
   private val initStates: Map<String, Int>
-  private val instructions: List<Instruction>
+  private val elements: List<Element>
 
   init {
     val (initStates, instructions) = input.toList("\n\n") { block -> block.toList { it } }
     this.initStates = initStates.associate { it.substringBefore(':') to it.substringAfter(' ').toInt() }
-    this.instructions = instructions.map { line ->
-      val (in1, cmd, in2, _, out) = line.split(' ')
-      Instruction(Command.valueOf(cmd), in1, in2, out)
+    this.elements = instructions.map { line ->
+      val (in1, logic, in2, _, out) = line.split(' ')
+      Element(Logic.valueOf(logic), in1, in2, out)
     }
   }
 
   override fun partA(): Long {
-    val outputs = instructions.associateBy { it.out }
+    val outputs = elements.associateBy { it.out }
     val states = initStates.toMutableMap()
     fun dfs(pin: String): Int {
       states[pin]?.let { return it }
@@ -35,7 +31,7 @@ class Year2024Day24(input: String) : Solution {
       val out = with(instruction) {
         val a = dfs(in1)
         val b = dfs(in2)
-        cmd.op(a, b)
+        logic.op(a, b)
       }
       return out.also { states[pin] = it }
     }
@@ -48,11 +44,79 @@ class Year2024Day24(input: String) : Solution {
   }
 
   /**
-   *  solved manually via graph picture rendered on python
-   *  @see [solutions/python/aoc/year_2024/day_24_graph.png]
+   * N - number of output pins
+   * Fi = Xi AND Yi for i>=1, R0 = X0 AND Y0
+   * Wi = Xi XOR Yi for i>=1, Z0 = X0 XOR Y0
+   * Vi = Wi AND R(i-1)
+   * Zi = Wi XOR R(i-1)
+   * Ri = Fi OR Vi
    */
   override fun partB(): Any {
-    val result = listOf("z14", "vss", "hjf", "kdh", "z31", "kpp", "z35", "sgj").sorted()
+    val outputs = elements.associateBy { it.out }
+    fun pin(c: Char, idx: Int) = "$c${idx.toString().padStart(2, '0')}"
+    val n = outputs.keys.filter { it.startsWith('z') }.max().substring(1).toInt()
+    val f = mutableMapOf<Int, Element>()
+    val w = mutableMapOf<Int, Element>()
+    val v = mutableMapOf<Int, Element>()
+    val r = mutableMapOf<Int, Element>()
+    val z = mutableMapOf<Int, Element>()
+    val crosses = elements.flatMap { listOf(it.in1, it.in2, it.out) }.associateWith { it }.toMutableMap()
+
+    fun find(inp: Pair<String, String>): List<Element> {
+      val (x1, x2) = crosses.getValue(inp.first) to crosses.getValue(inp.second)
+      return elements.filter { instr ->
+        with(instr) { (in1 == x1 && in2 == x2) || (in1 == x2 && in2 == x1) }
+      }
+    }
+
+    // init F, W, R[0] and Z[0] because they are based on input pins
+    for (i in 0..<n) {
+      val (x, y) = pin('x', i) to pin('y', i)
+      for (instr in find(x to y)) {
+        when (instr.logic) {
+          Logic.AND -> if (i > 0) f[i] = instr else r[0] = instr
+          Logic.XOR -> if (i > 0) w[i] = instr else z[0] = instr
+          Logic.OR -> error("Can't be OR")
+        }
+      }
+    }
+
+    // fill V, R, Z
+    for (i in 1..<n) {
+      val fi = checkNotNull(f[i]).out
+      val wi = checkNotNull(w[i]).out
+      val ri1 = checkNotNull(r[i - 1]).out
+      val possible = find(wi to ri1).takeIf { it.isNotEmpty() } ?: run { // Fi crossed with Wi
+        crosses[fi] = wi
+        crosses[wi] = fi
+        find(wi to ri1)
+      }
+      for (instr in possible) {
+        when (instr.logic) {
+          Logic.AND -> v[i] = instr
+          Logic.XOR -> {
+            z[i] = instr
+            val havePin = instr.out
+            val needPin = pin('z', i)
+            if (havePin != needPin) { // Z pin missmatch, make a cross
+              crosses[havePin] = needPin
+              crosses[needPin] = havePin
+            }
+          }
+          Logic.OR -> error("Can't be OR")
+        }
+      }
+
+      checkNotNull(z[i])
+      val vi = checkNotNull(v[i]).out
+      for (instr in find(fi to vi)) {
+        when (instr.logic) {
+          Logic.OR -> r[i] = instr
+          else -> error("Can't be AND / XOR")
+        }
+      }
+    }
+    val result = crosses.entries.filter { it.key != it.value }.map { it.key }.sorted()
     return result.joinToString(",")
   }
 
